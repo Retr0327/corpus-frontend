95d4cc96ff6f11fdb88b6d6add9f68f53a51def1
Retr0327 <philcoke35@gmail.com>
2022-09-25 12:35:27 +0800

    Add header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,29 @@
-import '../styles/globals.css';
+import Head from 'next/head';
+import { getCookie } from 'cookies-next';
 import type { AppProps } from 'next/app';
+import { ColorScheme } from '@mantine/core';
+import { GetServerSidePropsContext } from 'next';
+import DarkThemeContext from '@contexts/DarkTheme';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function App(props: AppProps & { colorScheme: ColorScheme }) {
+  const { Component, pageProps, colorScheme } = props;
+
+  return (
+    <>
+      <Head>
+        <title>Taiwan Social Media Corpus</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      <DarkThemeContext colorScheme={colorScheme}>
+        <Component {...pageProps} />;
+      </DarkThemeContext>
+    </>
+  );
 }
 
-export default MyApp;
+App.getInitialProps = ({ ctx }: { ctx: GetServerSidePropsContext }) => ({
+  colorScheme: getCookie('mantine-color-scheme', ctx) || 'light',
+});
+
+export default App;
