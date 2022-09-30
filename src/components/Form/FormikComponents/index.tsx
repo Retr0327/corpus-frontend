813aaad431791fdefb5d4814441e6b4aea175822
Retr0327 <philcoke35@gmail.com>
2022-10-01 00:46:52 +0800

    Add CheckboxGroup

--- src/components/Form/FormikComponents/index.tsx
+++ src/components/Form/FormikComponents/index.tsx
@@ -2,5 +2,6 @@ import Select from './Select';
 import TextInput from './TextInput';
 import RadioGroup from './RadioGroup';
 import MultiSelect from './MultiSelect';
+import CheckboxGroup from './CheckboxGroup';
 
-export { Select, TextInput, RadioGroup, MultiSelect };
+export { Select, TextInput, RadioGroup, MultiSelect, CheckboxGroup };
