6ea9402324120c2e8cd082b7cc87051eb30442b5
Retr0327 <philcoke35@gmail.com>
2022-10-02 15:30:08 +0800

    Add SelectItem

--- src/components/UI/index.tsx
+++ src/components/UI/index.tsx
@@ -1,4 +1,5 @@
 import Loader from './Loader';
+import SelectItem from './SelectItem';
 import ColorSchemeToggle from './ColorSchemeToggle';
 
-export { Loader, ColorSchemeToggle };
+export { Loader, SelectItem, ColorSchemeToggle };
