cb59ca8f784eff31c95d02f3422e39f9291ea69b
Retr0327 <philcoke35@gmail.com>
2022-10-02 13:04:45 +0800

    Add NumberInput

--- src/components/Form/FormikComponents/index.tsx
+++ src/components/Form/FormikComponents/index.tsx
@@ -3,6 +3,7 @@ import Switch from './Switch';
 import TextInput from './TextInput';
 import RadioGroup from './RadioGroup';
 import MultiSelect from './MultiSelect';
+import NumberInput from './NumberInput';
 import CheckboxGroup from './CheckboxGroup';
 
-export { Select, Switch, TextInput, RadioGroup, MultiSelect, CheckboxGroup };
+export { Select, Switch, TextInput, RadioGroup, MultiSelect, NumberInput, CheckboxGroup };
