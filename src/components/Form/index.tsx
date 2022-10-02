33042f8dd9f9ad0a9525d0ee09f2166b4b1bf3f0
Retr0327 <philcoke35@gmail.com>
2022-10-02 12:23:55 +0800

    Add `switch` control

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,6 +1,13 @@
 import { memo } from 'react';
 import { ControllerProps } from 'types';
-import { Select, TextInput, RadioGroup, MultiSelect, CheckboxGroup } from './FormikComponents';
+import {
+  Select,
+  Switch,
+  TextInput,
+  RadioGroup,
+  MultiSelect,
+  CheckboxGroup,
+} from './FormikComponents';
 
 function FormikController(props: ControllerProps) {
   const { control } = props;
@@ -8,6 +15,8 @@ function FormikController(props: ControllerProps) {
   switch (control) {
     case 'select':
       return <Select {...props} />;
+    case 'switch':
+      return <Switch {...props} />;
     case 'text-input':
       return <TextInput {...props} />;
     case 'radio-group':
