df920766bddd027276d57c75d31708511a2e7f65
Retr0327 <philcoke35@gmail.com>
2022-10-08 23:37:38 +0800

    Add FormValues

---
+++ src/containers/Corpus/types.ts
@@ -0,0 +1,11 @@
+export interface FormValues {
+  word: string;
+  cqlEnable: boolean;
+  media: string;
+  board: string;
+  start: number;
+  end: number;
+  postType: string;
+  windowSize: number;
+  fetchNumber: number;
+}
