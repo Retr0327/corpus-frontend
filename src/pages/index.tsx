c62379270ba1892db83bb578dca8d024e077c378
Retr0327 <philcoke35@gmail.com>
2022-10-09 20:51:53 +0800

    Update home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,61 @@
-import type { NextPage } from 'next';
+import getBoards from '@services/boards';
+import CorpusForm from '@containers/Form';
+import { GetServerSideProps } from 'next';
+import { ColorSchemeToggle } from '@components/UI';
+import { Container, Title, Group, Text, createStyles } from '@mantine/core';
 
-const Home: NextPage = () => <h1>Hello World</h1>;
+const useStyles = createStyles((theme) => ({
+  title: {
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    fontWeight: 900,
+    marginBottom: theme.spacing.md,
+    textAlign: 'center',
+
+    [theme.fn.smallerThan('sm')]: {
+      fontSize: 28,
+      textAlign: 'left',
+    },
+  },
+
+  description: {
+    textAlign: 'center',
+
+    [theme.fn.smallerThan('sm')]: {
+      textAlign: 'left',
+    },
+  },
+}));
+
+type Board = { boards: { [key in string]: any } };
+
+function Home({ boards }: Board) {
+  const { classes } = useStyles();
+
+  return (
+    <Container size={700} my={40}>
+      <Group position="right" mt={5}>
+        <ColorSchemeToggle />
+      </Group>
+      <Title className={classes.title}>Taiwan Social Media Corpus</Title>
+      <Container size={560} p={0}>
+        <Text size="sm" className={classes.description}>
+          A corpus of PTT and Dcard.
+        </Text>
+      </Container>
+      <CorpusForm boards={boards} />
+    </Container>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps<Board> = async () => {
+  const redirect = { redirect: { permanent: false, destination: '/500' } };
+  const [result, error] = await getBoards();
+
+  if (error || result.status === 'failed') {
+    return redirect;
+  }
+
+  return { props: { boards: result.data } };
+};
 
 export default Home;
