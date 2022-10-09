034c342868081e4a8f5337497c4b3f75317aec37
Retr0327 <philcoke35@gmail.com>
2022-10-09 20:53:37 +0800

    Add types

---
+++ src/containers/Form/types.ts
@@ -0,0 +1,11 @@
+export interface FormValues {
+  word: string;
+  cqlEnable: boolean;
+  media: string;
+  boards: string;
+  start: number;
+  end: number;
+  postType: string;
+  windowSize: number;
+  fetchNumber: number;
+}
