aef66a50972a24b754777aef83f084668c60d8c4
Retr0327 <philcoke35@gmail.com>
2022-10-09 20:24:36 +0800

    Add options

---
+++ src/containers/Corpus/options.ts
@@ -0,0 +1,37 @@
+const mediaOptions = [
+  { label: 'All', value: 'all' },
+  { label: 'PTT', value: 'ptt', image: '/ptt-logo.png' },
+  { label: 'Dcard', value: 'dcard', image: '/dcard-logo.png' },
+];
+
+function generatePostTypeOptions(media: string) {
+  const options = [
+    { label: 'All', value: 'all' },
+    { label: 'Title', value: 'title' },
+    { label: 'Body', value: 'body' },
+    { label: 'Comment', value: 'commentAll' },
+  ];
+
+  switch (media) {
+    case 'ptt': {
+      const pttOptions = [
+        { label: 'commentPos', value: 'commentPos' },
+        { label: 'commentNeg', value: 'commentNeg' },
+        { label: 'commentNeu', value: 'commentNeu' },
+      ];
+      return options.concat(pttOptions);
+    }
+    case 'dcard': {
+      const dcardOptions = [
+        { label: 'commentPos', value: 'commentPos' },
+        { label: 'commentMale', value: 'commentMale' },
+        { label: 'commentFemale', value: 'commentFemale' },
+      ];
+      return options.concat(dcardOptions);
+    }
+    default:
+      return options;
+  }
+}
+
+export { mediaOptions, generatePostTypeOptions };
