5e90ca8f1913eb07b65ea10aba1610e89c205b76
Retr0327 <philcoke35@gmail.com>
2022-10-10 12:35:45 +0800

    Add BlacklabResponse

--- src/types/index.ts
+++ src/types/index.ts
@@ -98,3 +98,98 @@ export type ControllerProps =
 export type ControllerPropsWithCol = {
   controllers: (ControllerProps & { col?: ColProps })[];
 };
+
+export interface BlacklabResponse {
+  summary: Summary;
+  hits: Hit[] | [];
+  docInfos: DocInfos | {};
+}
+
+export interface Hit {
+  docPid: string;
+  start: number;
+  end: number;
+  left: HitData;
+  match: HitData;
+  right: HitData;
+}
+
+export interface HitData {
+  punct: string[];
+  pos: string[];
+  word: string[];
+}
+
+export interface DocInfos {
+  [key: string]: {
+    fromInputFile: string[];
+    year: string[];
+    author: string[];
+    media: string[];
+    doc_id: string[];
+    board: string[];
+    lengthInTokens: number;
+    mayView: boolean;
+  };
+}
+
+export interface Summary {
+  searchParam: SearchParam;
+  searchTime: number;
+  countTime: number;
+  windowFirstResult: number;
+  requestedWindowSize: number;
+  actualWindowSize: number;
+  windowHasPrevious: boolean;
+  windowHasNext: boolean;
+  stillCounting: boolean;
+  numberOfHits: number;
+  numberOfHitsRetrieved: number;
+  stoppedCountingHits: boolean;
+  stoppedRetrievingHits: boolean;
+  numberOfDocs: number;
+  numberOfDocsRetrieved: number;
+  docFields: DocFields;
+  metadataFieldDisplayNames: MetadataFieldDisplayNames;
+}
+
+export interface DocFields {
+  pidField: string;
+  titleField: string;
+  authorField: string;
+  dateField: string;
+}
+
+export interface MetadataFieldDisplayNames {
+  author: string;
+  board: string;
+  doc_id: string;
+  fromInputFile: string;
+  media: string;
+  year: string;
+}
+
+export interface SearchParam {
+  filter: string;
+  first: string;
+  indexname: string;
+  number: string;
+  patt: string;
+  wordsaroundhit: string;
+}
+
+export type Boards = {
+  indexName: string;
+  fieldName: string;
+  isAnnotatedField: boolean;
+  displayName: string;
+  description: string;
+  uiType: string;
+  type: string;
+  analyzer: string;
+  unknownCondition: string;
+  unknownValue: string;
+  displayValues: { [key: string]: any };
+  fieldValues: { [key: string]: number };
+  valueListComplete: boolean;
+};
