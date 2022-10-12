51cc347ab6268032fe89507d7d4f8e8f7e69ab74
Retr0327 <philcoke35@gmail.com>
2022-10-12 16:25:21 +0800

    Update concordance

--- src/pages/concordance.tsx
+++ src/pages/concordance.tsx
@@ -1,30 +1,13 @@
-import decode from '@utils/url';
-import { CorpusQueries } from 'types';
 import { useRouter } from 'next/router';
-import { GetServerSideProps } from 'next';
+import { NextPage, GetServerSideProps } from 'next';
 import ConcordanceTable from '@containers/Concordance';
 
-function Concordance() {
+const Concordance: NextPage = () => {
   const router = useRouter();
   const { e, page } = router.query as { e: string; page: string };
-  const decoded = decode(e);
-  const params = new URLSearchParams(decoded as string);
 
-  const payload = {
-    word: params.get('w'),
-    media: params.get('m') === 'all' ? '' : params.get('m'),
-    cqlEnable: params.get('c') === 'true',
-    postType: params.get('p'),
-    boards: params.get('b'),
-    start: params.get('s'),
-    end: params.get('e'),
-    windowSize: params.get('win'),
-    page: Number(page),
-    fetchNumber: Number(params.get('f')),
-  } as CorpusQueries;
-
-  return <ConcordanceTable payload={payload} />;
-}
+  return <ConcordanceTable page={page} e={e} />;
+};
 
 export default Concordance;
 
