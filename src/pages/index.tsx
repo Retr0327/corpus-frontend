c00063a8892c94781ce2f8042965c203c12e66df
Retr0327 <philcoke35@gmail.com>
2022-10-12 15:43:33 +0800

    Update Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,50 +1,11 @@
 import getBoards from '@services/boards';
 import CorpusForm from '@containers/Form';
 import { GetServerSideProps } from 'next';
-import { ColorSchemeToggle } from '@components/UI';
-import { Container, Title, Group, Text, createStyles } from '@mantine/core';
-
-const useStyles = createStyles((theme) => ({
-  title: {
-    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-    fontWeight: 900,
-    marginBottom: theme.spacing.md,
-    textAlign: 'center',
-
-    [theme.fn.smallerThan('sm')]: {
-      fontSize: 28,
-      textAlign: 'left',
-    },
-  },
-
-  description: {
-    textAlign: 'center',
-
-    [theme.fn.smallerThan('sm')]: {
-      textAlign: 'left',
-    },
-  },
-}));
 
 type Board = { boards: { [key in string]: any } };
 
 function Home({ boards }: Board) {
-  const { classes } = useStyles();
-
-  return (
-    <Container size={700} my={40}>
-      <Group position="right" mt={5}>
-        <ColorSchemeToggle />
-      </Group>
-      <Title className={classes.title}>Taiwan Social Media Corpus</Title>
-      <Container size={560} p={0}>
-        <Text size="sm" className={classes.description}>
-          A corpus of PTT and Dcard.
-        </Text>
-      </Container>
-      <CorpusForm boards={boards} />
-    </Container>
-  );
+  return <CorpusForm boards={boards} />;
 }
 
 export const getServerSideProps: GetServerSideProps<Board> = async () => {
