28bc7c334a7cacbdf98c322ea3062ba7ef23cf5a
Retr0327 <philcoke35@gmail.com>
2022-10-22 10:53:52 +0800

    Remove externalApi

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,16 +1,11 @@
 const api = process.env.NEXT_PUBLIC_API_SERVICE;
-const externalApi = process.env.NEXT_PUBLIC_EXTERNAL_API_SERVICE;
 
 if (!api) {
   throw new Error('API_SERVICE undefined');
 }
 
-if (!externalApi) {
-  throw new Error('EXTERNAL_API_SERVICE undefined');
-}
-
 const corpus = `${api}/corpus`;
 
-const API = { corpus, csv: `${corpus}/csv`, boards: `${externalApi}/corpus/boards` } as const;
+const API = { corpus, csv: `${corpus}/csv`, boards: `${corpus}/boards` } as const;
 
 export default API;
