c7f1d4bd3b45d8f2d59d9e9ddb9a39c657e80bfe
Retr0327 <philcoke35@gmail.com>
2022-10-23 00:06:18 +0800

    Add MediaCardProps

--- src/containers/Concordance/Table/MediaCard/types.ts
+++ src/containers/Concordance/Table/MediaCard/types.ts
@@ -3,3 +3,11 @@ export type PostLinkProps = {
   docId: string;
   board: string;
 };
+
+export type MediaCardProps = {
+  author: string;
+  board: string;
+  docId: string;
+  media: string;
+  year: string;
+};
