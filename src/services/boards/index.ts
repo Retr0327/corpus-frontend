1a8ad145e32a7cadc494cd0beac4b6fe13e3d868
Retr0327 <philcoke35@gmail.com>
2022-10-22 15:16:01 +0800

    Update response type

--- src/services/boards/index.ts
+++ src/services/boards/index.ts
@@ -1,10 +1,14 @@
 import useSWR from 'swr';
 import API from '@config/api';
-import { Boards } from 'types';
 import request from '@utils/request';
+import { Response, Boards } from 'types';
+
+type ResponseType = Response<Boards> | null;
 
 const getBoards = () => {
-  const { data, error } = useSWR<Boards>(API.boards, (url) => request({ url, method: 'GET' }));
+  const { data, error } = useSWR<ResponseType>(API.boards, (url) =>
+    request({ url, method: 'GET' })
+  );
 
   return { boards: data, isLoading: !error && !data, isError: error };
 };
