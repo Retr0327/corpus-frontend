c5aca99e3e086752400a1075dacb0dbb97af1b96
Retr0327 <philcoke35@gmail.com>
2022-10-23 01:26:19 +0800

    Add `showPos` field

--- src/containers/Concordance/Pagination/types.ts
+++ src/containers/Concordance/Pagination/types.ts
@@ -6,6 +6,7 @@ type ResponseType = Response<BlacklabResponse> | null;
 export type Props = {
   e: string;
   page: number;
+  showPos: boolean;
   fetchNumber: number;
   numberOfHits: number;
   mutate: KeyedMutator<ResponseType>;
