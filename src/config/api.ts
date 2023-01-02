2f3552c3d84f61eb5e41b1ad1d410420f47ba856
Retr0327 <philcoke35@gmail.com>
2023-01-03 01:18:59 +0800

    Update api urls

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,11 +1,32 @@
-const api = process.env.NEXT_PUBLIC_API_SERVICE;
+import urlJoin from 'url-join';
 
-if (!api) {
-  throw new Error('API_SERVICE undefined');
+const isProduction = process.env.NODE_ENV === 'production';
+
+// ----- version tag -----
+const V1 = 'v1';
+
+// ------ api urls ------
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const EXTERNAL_API_URL = process.env.NEXT_PUBLIC_EXTERNAL_API_URL;
+
+if (!API_URL || !EXTERNAL_API_URL) {
+  throw new Error('api urls undefined');
 }
 
-const corpus = `${api}/corpus`;
+const api = isProduction ? `/service/api/${V1}` : API_URL;
+const externalAPI = isProduction ? `http://nginx:80/service/api/${V1}` : EXTERNAL_API_URL;
 
-const API = { corpus, csv: `${corpus}/csv`, boards: `${corpus}/boards` } as const;
+// ---- corpus service ----
+const corpus = urlJoin(api, 'corpus');
+const externalCorpus = urlJoin(externalAPI, 'corpus');
+
+const API = {
+  V1: {
+    corpus: {
+      boards: { root: urlJoin(corpus, 'boards'), external: urlJoin(externalCorpus, 'boards') },
+      concordance: urlJoin(corpus, 'concordance'),
+    },
+  },
+};
 
 export default API;
