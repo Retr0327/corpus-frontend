bf88b7ae5307c8c82ddd7c6a5e9c1e46dc6df1a6
Retr0327 <philcoke35@gmail.com>
2023-01-03 11:47:14 +0800

    Add `select` control

--- src/components/common/form/index.tsx
+++ src/components/common/form/index.tsx
@@ -1,11 +1,13 @@
 import { memo } from 'react';
 import { ControllerProps } from 'types/form';
-
+import { Select, TextInput } from './components';
 
 function FormController(props: ControllerProps) {
   const { control } = props;
 
   switch (control) {
+    case 'select':
+      return <Select {...props} />;
     case 'text-input':
       return <TextInput {...props} />;
     default:
