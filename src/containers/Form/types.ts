3205e60b6a089ad66af3a4bac034a5635a0cfee1
Retr0327 <philcoke35@gmail.com>
2023-01-03 11:57:48 +0800

    Add CorpusForm

--- src/containers/Form/types.ts
+++
@@ -1,7 +0,0 @@
-import { CorpusQueries } from 'types';
-
-export type FormValues = Omit<CorpusQueries, 'page' | 'start' | 'end' | 'windowSize'> & {
-  start: number;
-  end: number;
-  windowSize: number;
-};
