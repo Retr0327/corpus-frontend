472212a0fc3e6acd3a64af9719859cf2621fa458
Retr0327 <philcoke35@gmail.com>
2023-01-03 11:45:43 +0800

    Add NumberInput

--- src/components/common/form/components/index.tsx
+++ src/components/common/form/components/index.tsx
@@ -2,5 +2,6 @@ import Select from './Select';
 import Switch from './Switch';
 import TextInput from './TextInput';
 import MultiSelect from './MultiSelect';
+import NumberInput from './NumberInput';
 
-export { Select, Switch, TextInput, MultiSelect };
+export { Select, Switch, TextInput, MultiSelect, NumberInput };
